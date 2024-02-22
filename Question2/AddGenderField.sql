60c0c3cf51a3f2484b6c2bd2283a3106ab40c5b9
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-22 20:22:54 +0530

    SQL Loops. <Sigh>...

--- Question2/AddGenderField.sql
+++ Question2/AddGenderField.sql
@@ -16,3 +16,34 @@ UPDATE users_table
 SET gender = 'F'
 WHERE id = 2;
 -- Apparently we have loops, too.
+UPDATE users_table
+SET gender = 'M'
+WHERE id = 3;
+-- But that stuff makes SQL Turing complete (or something like that!)
+UPDATE users_table
+SET gender = 'M'
+WHERE id = 4;
+-- And the syntax is terrible!
+UPDATE users_table
+SET gender = 'F'
+WHERE id = 5;
+-- SQL was way ahead of its time.
+UPDATE users_table
+SET gender = 'M'
+WHERE id = 6;
+-- The syntax had to be something. It had to work.
+UPDATE users_table
+SET gender = 'M'
+WHERE id = 7;
+-- It sucks, so I'm not using it, but...
+UPDATE users_table
+SET gender = 'M'
+WHERE id = 8;
+-- Just so you know - we COULD'VE been *not* doing this!
+UPDATE users_table
+SET gender = 'F'
+WHERE id = 9;
+-- ...Alright, we're done.
+UPDATE users_table
+SET gender = 'M'
+WHERE id = 10;
