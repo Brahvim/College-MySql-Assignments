970cbac2c5d9076b3df19309c2e6c37000be686b
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-22 11:06:37 +0530

    Finally completed this LOL.

--- Question2/Question2.sql
+++ Question2/Question2.sql
@@ -1,30 +1,33 @@
--- Note: This script has too many syntax errors right now.
-
-
-
 -- Creating the DB:
 CREATE SCHEMA soc_net_db;
 
 --  Creating aaaaaall three tables:
 CREATE TABLE users_table(
-        id AUTO INCREMENT,
-        PRIMARY KEY field username VARCHAR(60),
-        email VARCHAR(255),
-        address VARCHAR(150),
-        dob TIMESTAMP,
-        is_active TINYINT,
-        registered_on TIMESTAMP,
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_address VARCHAR(150),
         last_logged_on TIMESTAMP
+        registered_on TIMESTAMP,
+        username VARCHAR(60),
+        email VARCHAR(255),
+        is_active TINYINT,
+        dob TIMESTAMP,
     );
 
-CREATE TABLE users_table(
-        id AUTO INCREMENT,
-        PRIMARY KEY field user_id unsigned INT,
-        NOT NULL friend_name(60),
+CREATE TABLE friends_table(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT UNSIGNED NOT NULL,
+        friend_name VARCHAR(60),
     );
 
-CREATE TABLE users_table(
-        id user_id location,
+CREATE TABLE profile_table(
+    user_id INT,
+    id INT PRIMARY KEY,
+    user_location VARCHAR(30),
     );
 
 
+-- Describe the tables:
+
+DESCRIBE users_table;
+DESCRIBE friends_table;
+DESCRIBE profiles_table;
